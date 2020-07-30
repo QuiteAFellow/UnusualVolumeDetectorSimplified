abade34ed4b2ee57ed2f3b23dc46ef4326b3d897
QuiteAFellow <69014551+QuiteAFellow@users.noreply.github.com>
2020-07-30 15:44:37 -0400

    Add files via upload

---
+++ MarketScannerSimplified.py
@@ -0,0 +1,148 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+import os
+import time
+import yfinance as yf
+import dateutil.relativedelta
+from datetime import date
+import datetime
+import numpy as np
+import sys
+from tqdm import tqdm
+from ftplib import FTP
+import os
+import errno
+
+exportList = []
+
+# Start of stocklist code
+class NasdaqController:
+    def getList(self):
+        return exportList
+
+    def __init__(self, update=True):
+
+        self.filenames = {
+            "otherlisted": "data/otherlisted.txt",
+            "nasdaqlisted": "data/nasdaqlisted.txt"
+        }
+
+        # Update lists only if update = True
+
+        if update == True:
+            self.ftp = FTP("ftp.nasdaqtrader.com")
+            self.ftp.login()
+
+            #print("Nasdaq Controller: Welcome message: " + self.ftp.getwelcome())
+
+            self.ftp.cwd("SymbolDirectory")
+
+            for filename, filepath in self.filenames.items():
+                if not os.path.exists(os.path.dirname(filepath)):
+                    try:
+                        os.makedirs(os.path.dirname(filepath))
+                    except OSError as exc:  # Guard against race condition
+                        if exc.errno != errno.EEXIST:
+                            raise
+
+                self.ftp.retrbinary("RETR " + filename +
+                                    ".txt", open(filepath, 'wb').write)
+
+        all_listed = open("data/alllisted.txt", 'w')
+
+        for filename, filepath in self.filenames.items():
+            with open(filepath, "r") as file_reader:
+                for i, line in enumerate(file_reader, 0):
+                    if i == 0:
+                        continue
+
+                    line = line.strip().split("|")
+
+                    if line[0] == "" or line[1] == "":
+                        continue
+
+                    all_listed.write(line[0] + ",")
+                    global exportList
+                    exportList.append(line[0])
+                    all_listed.write(line[0] + "|" + line[1] + "\n")
+
+# End of stocklist code
+# *****************************************************************************************
+
+#Start of Market Scanner
+
+class mainObj:
+    def getData(self, ticker):
+        currentDate = datetime.datetime.strptime(
+            date.today().strftime("%Y-%m-%d"), "%Y-%m-%d")
+        pastDate = currentDate - dateutil.relativedelta.relativedelta(months=5)
+        sys.stdout = open(os.devnull, "w")
+        data = yf.download(ticker, pastDate, currentDate)
+        sys.stdout = sys.__stdout__
+        return data[["Volume"]]
+
+    def find_anomalies(self, data, cutoff):
+        anomalies = []
+        data_std = np.std(data['Volume'])
+        data_mean = np.mean(data['Volume'])
+        anomaly_cut_off = data_std * cutoff
+        upper_limit = data_mean + anomaly_cut_off
+        indexs = data[data['Volume'] > upper_limit].index.tolist()
+        outliers = data[data['Volume'] > upper_limit].Volume.tolist()
+        index_clean = [str(x)[:-9] for x in indexs]
+        d = {'Dates': index_clean, 'Volume': outliers}
+        return d
+
+    def find_anomalies_two(self, data, cutoff):
+        indexs = []
+        outliers = []
+        data_std = np.std(data['Volume'])
+        data_mean = np.mean(data['Volume'])
+        anomaly_cut_off = data_std * cutoff
+        upper_limit = data_mean + anomaly_cut_off
+        data.reset_index(level=0, inplace=True)
+        for i in range(len(data)):
+            temp = data['Volume'].iloc[i]
+            if temp > upper_limit:
+                indexs.append(str(data['Date'].iloc[i])[:-9])
+                outliers.append(temp)
+        d = {'Dates': indexs, 'Volume': outliers}
+        return d
+
+    def customPrint(self, d, tick):
+        print("\n\n\n*******  " + tick.upper() + "  *******")
+        print("Ticker is: "+tick.upper())
+        for i in range(len(d['Dates'])):
+            str1 = str(d['Dates'][i])
+            str2 = str(d['Volume'][i])
+            print(str1 + " - " + str2)
+        print("*********************\n\n\n")
+
+    def days_between(self, d1, d2):
+        d1 = datetime.datetime.strptime(d1, "%Y-%m-%d")
+        d2 = datetime.datetime.strptime(d2, "%Y-%m-%d")
+        return abs((d2 - d1).days)
+
+    def main_func(self, cutoff):
+        StocksController = NasdaqController(True)
+        list_of_tickers = StocksController.getList()
+        currentDate = datetime.datetime.strptime(
+            date.today().strftime("%Y-%m-%d"), "%Y-%m-%d")
+        start_time = time.time()
+        for x in tqdm(list_of_tickers):
+            d = (self.find_anomalies_two(self.getData(x), cutoff))
+            if d['Dates']:
+                for i in range(len(d['Dates'])):
+                    if self.days_between(str(currentDate)[:-9], str(d['Dates'][i])) <= 3:
+                        self.customPrint(d, x)
+
+        print("\n\n\n\n--- this took %s seconds to run ---" %
+              (time.time() - start_time))
+
+
+# input desired anomaly standard deviation cuttoff
+# run time around 50 minutes for every single ticker.
+mainObj().main_func(10)
+
+
